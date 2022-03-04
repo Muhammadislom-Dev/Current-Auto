f4c1b488b3f62ec15622a331b9fe95e8cdac74cc
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-03-04 11:17:58 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
- 
 import HomePages from './Pages/HomePages/HomePages';
 import Inventory from './Pages/Inventory/Inventory';
+import Pictures from './Pages/Pictures/Pictures';
 
 function App() {
   return (
     <div className="App">
       <Switch>
       <Route path="/inventory" component={Inventory} />
-      <Route path="/" component={HomePages} />
+      <Route path="/" component={HomePages} exact />
+      <Route path="/pictures" component={Pictures} />
       </Switch>
     </div>
   );
